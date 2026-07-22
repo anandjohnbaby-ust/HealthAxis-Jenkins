ea04bdb4586de1845b1ecac9bd4bcc7f05e56918
anandjohnbaby-ust <310052@ust.com>
2026-07-23 00:42:52 +0530

    Made Changes in Angular for Deployment

--- HealthAxis.UI/src/environments/environment.development.ts
+++ HealthAxis.UI/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.development.ts
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:7207/api',
+  apiBaseUrl: '/api',
   blazorAdminUrl: 'https://localhost:7197'
 };
--- HealthAxis.UI/src/environments/environment.ts
+++ HealthAxis.UI/src/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://your-prod-api.com/api',
-  blazorAdminUrl: 'https://your-prod-blazor-admin.com'
+  apiBaseUrl: '/api',
+  blazorAdminUrl: '/blazor'
 };
